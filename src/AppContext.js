226f4f20b7088b02f2a2a25d7d297e47c6b78c4a
Maciej Rybak <maciek.rybak95@gmail.com>
2022-11-16 20:08:03 +0100

    Clean react app

--- src/AppContext.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-
-export const defaultObject = {
-  isUserLogged: false,
-  toggleLoggedState: () => console.log('Domyślny Provider'),
-}
-
-export const AppContext = createContext(defaultObject);
